605b19780a72c5410fb26159e7d14b36eba73eca
Mislav Bradac <mislav.bradac@gmail.com>
2020-04-04 19:18:18 +0200

    Rename confirmed to mild in plot.py

--- plot.py
+++ plot.py
@@ -10,6 +10,11 @@ with open(sys.argv[1]) as f:
 with open(sys.argv[2]) as f:
     data = json.load(f)
 
+# "mild" is better name than "confirmed" since people in "icu" state are also
+# confirmed cases in the real world
+data["mild"] = data["confirmed"]
+del data["confirmed"]
+
 for state in data:
     plt.plot(np.arange(len(data[state])), data[state], label=state)
 for event in config["simulation"]["events"]:
